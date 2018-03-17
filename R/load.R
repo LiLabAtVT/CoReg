a624b587b13ed0ce5beeded5afafd32b7ead0941
alexQiSong <alexsong@vt.edu>
2018-03-17 09:14:38 -0400

    Add networkFromEdgeList() for converting data frame to graph

--- R/load.R
+++ R/load.R
@@ -4,7 +4,7 @@
 #########################################################
 # library(igraph)
 
-loadNetwork<-function(file,sep = "",header = F,simple = T){
+networkFromFile<-function(file,sep = "",header = F,simple = T){
     el<-read.table(file=file,sep=sep,header=header,as.is=T,quote="")
     if(ncol(el)!=2){
       stop("Input file should contain two columns. Please check the format")
@@ -19,6 +19,19 @@ loadNetwork<-function(file,sep = "",header = F,simple = T){
     }
 }
 
+networkFromEdgeList(edgeList,simple = T){
+    if(ncol(edgeList)!=2 && !is.data.frame(edgeList)){
+        stop("edgeList should be a data frame containing two columns. Please check the input")
+    }
+    g<-graph.edgelist(as.matrix(edgeList),directed = T)
+    # Remove self loop and multiple edges
+    if(simple == T){
+        g<-simplify(g)
+        return(g)
+    }else if(!is(simple,"logical")){
+        stop("Argument 'simple' should be 'True' or 'False'!")
+    }
+}
 #########################################################
 # Convert a directed network to a bipartite one
 # Note this is actually stored as undirected igraph
